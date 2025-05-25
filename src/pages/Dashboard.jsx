acfc7553649f35cd536a1ff9ed922b0bb0b71791
Aravinth <aravinth2721@gmail.com>
2025-05-25 21:43:46 +0530

    Dashboard header section completed

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,30 @@
+import styles from "../styles/DashboardMainContent/Dashboard.module.css";
+import { MdNotifications, MdSearch, MdKeyboardArrowDown } from "react-icons/md";
+import CalenderView from "../components/DashboardMainContent/CalendarView";
+
 function Dashboard() {
   return (
-    <>
-      <h1>Dashboard Page</h1>
-    </>
+    <section className={styles.dashboard}>
+      <section>
+        <div className={styles.dashboardTop}>
+          <div className={styles.searchBar}>
+            <MdSearch size={20} className={styles.searchIcon} />
+            <input type="search" placeholder="Search" />
+          </div>
+          <MdNotifications size={24} className={styles.notificationIcon} />
+        </div>
+        <div className={styles.title}>
+          <p>Dashboard</p>
+          <div className={styles.detailSection}>
+            <span>Details</span>
+            <MdKeyboardArrowDown />
+          </div>
+        </div>
+      </section>
+      <section>
+        <CalenderView />
+      </section>
+    </section>
   );
 }
 export default Dashboard;
