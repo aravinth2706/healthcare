64b5ccc943e529c6f5393897deee495c5ee4a1fa
Aravinth <aravinth2721@gmail.com>
2025-05-26 20:26:08 +0530

    Activity section completed

--- src/components/DashboardMainContent/ActivityFeed.jsx
+++ src/components/DashboardMainContent/ActivityFeed.jsx
@@ -1,8 +1,26 @@
+import styles from "../../styles/DashboardMainContent/ActivityFeed.module.css";
+
+const dayLists = ["Mon", "Tues", "Wed", "Thu", "Fri", "Sat", "Sun"];
+const barCount = 15;
+
 function ActivityFeed() {
   return (
-    <>
-      <p>ActivityFeed</p>
-    </>
+    <section className={styles.activityContainer}>
+      <div className={styles.activityHeader}>
+        <p>Activity</p>
+        <p>3 appoinment on this week</p>
+      </div>
+      <div className={styles.equalizer}>
+        {Array.from({ length: barCount }).map((_, index) => (
+          <div key={index} className={styles.bar}></div>
+        ))}
+      </div>
+      <div className={styles.dayLists}>
+        {dayLists.map((day, index) => (
+          <p key={index}>{day}</p>
+        ))}
+      </div>
+    </section>
   );
 }
 export default ActivityFeed;
--- src/components/DashboardMainContent/DashboardOverview/AnatomySection.jsx
+++ src/components/DashboardMainContent/DashboardOverview/AnatomySection.jsx
@@ -2,25 +2,30 @@ import anatomy from "../../../assets/images/dashboard/human-anatomy.png";
 import styles from "../../../styles/DashboardMainContent/DashboardOverview/Anatomy.module.css";
 import { MdFavorite, MdDirectionsRun, MdZoomIn } from "react-icons/md";
 import HealthStatusCards from "./HealthStatusCards";
+import ActivityFeed from "../ActivityFeed";
+
 function AnatomySection() {
   return (
-    <section className={styles.dashboardMain}>
-      <div className={styles.anatomyContainer}>
-        <div className={styles.anatomy}>
-          <img src={anatomy} alt="Anatomy" height={320} />
-          <div className={styles.heart}>
-            <MdFavorite color="red" />
-            Healthly Heart
+    <>
+      <section className={styles.dashboardMain}>
+        <div className={styles.anatomyContainer}>
+          <div className={styles.anatomy}>
+            <img src={anatomy} alt="Anatomy" height={320} />
+            <div className={styles.heart}>
+              <MdFavorite color="red" />
+              Healthly Heart
+            </div>
+            <div className={styles.leg}>
+              <MdDirectionsRun color="white" />
+              Healthly Leg
+            </div>
+            <MdZoomIn size={20} className={styles.zoomInIcon} />
           </div>
-          <div className={styles.leg}>
-            <MdDirectionsRun color="white" />
-            Healthly Leg
-          </div>
-          <MdZoomIn size={20} className={styles.zoomInIcon} />
         </div>
-      </div>
-      <HealthStatusCards />
-    </section>
+        <HealthStatusCards />
+      </section>
+      <ActivityFeed />
+    </>
   );
 }
 export default AnatomySection;
