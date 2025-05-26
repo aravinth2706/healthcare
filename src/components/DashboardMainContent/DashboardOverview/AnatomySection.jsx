2a86c7167d8e4947d77d0b05d8fc28d769c1d1ea
Aravinth <aravinth2721@gmail.com>
2025-05-26 12:39:23 +0530

    Healthstatus and Anatomy section completed

--- src/components/DashboardMainContent/DashboardOverview/AnatomySection.jsx
+++ src/components/DashboardMainContent/DashboardOverview/AnatomySection.jsx
@@ -1,12 +1,13 @@
 import anatomy from "../../../assets/images/dashboard/human-anatomy.png";
 import styles from "../../../styles/DashboardMainContent/DashboardOverview/Anatomy.module.css";
 import { MdFavorite, MdDirectionsRun, MdZoomIn } from "react-icons/md";
+import HealthStatusCards from "./HealthStatusCards";
 function AnatomySection() {
   return (
     <section className={styles.dashboardMain}>
       <div className={styles.anatomyContainer}>
         <div className={styles.anatomy}>
-          <img src={anatomy} />
+          <img src={anatomy} alt="Anatomy" height={320} />
           <div className={styles.heart}>
             <MdFavorite color="red" />
             Healthly Heart
@@ -18,7 +19,7 @@ function AnatomySection() {
           <MdZoomIn size={20} className={styles.zoomInIcon} />
         </div>
       </div>
-      <div className={styles.healthStatus}></div>
+      <HealthStatusCards />
     </section>
   );
 }
--- src/components/DashboardMainContent/DashboardOverview/HealthStatusCards.jsx
+++ src/components/DashboardMainContent/DashboardOverview/HealthStatusCards.jsx
@@ -1,8 +1,47 @@
+import styles from "../../../styles/DashboardMainContent/DashboardOverview/HealthStatusCards.module.css";
+import lungs from "../../../assets/images/dashboard/lungs.png";
+import teeth from "../../../assets/images/dashboard/teeth.png";
+import bone from "../../../assets/images/dashboard/bone.png";
+import { MdArrowForward } from "react-icons/md";
+
 function HealthStatusCards() {
   return (
-    <>
-      <p>HealthStatusCards</p>
-    </>
+    <section className={styles.healthStatusContainer}>
+      <div className={styles.healthStatus}>
+        <div>
+          <img src={lungs} alt="Lungs" />
+          <p>Lungs</p>
+        </div>
+        <p className={styles.date}>Date 26 Oct 2021</p>
+        <div className={styles.progressContainer}>
+          <div style={{ backgroundColor: "#b34e50" }} />
+        </div>
+      </div>
+      <div className={styles.healthStatus}>
+        <div>
+          <img src={teeth} alt="Teeth" />
+          <p>Teeth</p>
+        </div>
+        <p className={styles.date}>Date 26 Oct 2021</p>
+        <div className={styles.progressContainer}>
+          <div style={{ backgroundColor: "#7FD3C4" }} />
+        </div>
+      </div>
+      <div className={styles.healthStatus}>
+        <div>
+          <img src={bone} alt="Bone" />
+          <p>Bone</p>
+        </div>
+        <p className={styles.date}>Date 26 Oct 2021</p>
+        <div className={styles.progressContainer}>
+          <div style={{ backgroundColor: "#F87155" }} />
+        </div>
+      </div>
+      <div className={styles.details}>
+        <p>Details</p>
+        <MdArrowForward />
+      </div>
+    </section>
   );
 }
 export default HealthStatusCards;
